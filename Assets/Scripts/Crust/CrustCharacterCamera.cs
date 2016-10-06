9370bfd68191a820bac4c89fea0ce43fc6cc1359
Jack McCarron <jack.g.mccarron@gmail.com>
2016-10-05 19:33:55 -0700

    camera rotation deadzone

--- Assets/Scripts/Crust/CrustCharacterCamera.cs
+++ Assets/Scripts/Crust/CrustCharacterCamera.cs
@@ -9,6 +9,7 @@ namespace AssemblyCSharp
 		public float Height = 2.0f;
 		public float RotationDegreesPerSecond = 180.0f;
 		public float RotationDeceleration = 10.0f;
+		public float RotationDeadZone = 45.0f;
 
 		public GameObject CharacterTarget;
 
@@ -37,35 +38,51 @@ namespace AssemblyCSharp
 			{
 				//get the camera direction
 				Vector2 cameraDirection = new Vector2(this.transform.forward.x, this.transform.forward.z).normalized;
+				Vector2 cameraBackDirection = -1 * cameraDirection;
 
 				//get the turn direction (it's already in world space relative to the camera)
 				Vector2 turnDirection = this._machine.LastTurnDirection;
 
-				//get the angle difference between them
+				//get the camera angle and the target angle
 				float targetAngle = Mathf.Atan2(turnDirection.y, turnDirection.x);
 				float currentAngle = Mathf.Atan2(cameraDirection.y, cameraDirection.x);
-				float angleDifference = currentAngle - targetAngle;
 
-				//wrap it if necessary (should never happen)
-				if (angleDifference > Mathf.PI)
-					angleDifference = angleDifference - Mathf.PI * 2.0f;
+				//DEBUG
+				Debug.DrawRay(this._target.position, Quaternion.AngleAxis(this.RotationDeadZone, Vector3.up) * new Vector3(cameraBackDirection.x, 0.0f, cameraBackDirection.y), Color.red);
+				Debug.DrawRay(this._target.position, Quaternion.AngleAxis(-this.RotationDeadZone, Vector3.up) * new Vector3(cameraBackDirection.x, 0.0f, cameraBackDirection.y), Color.red);
 
-				if (angleDifference < -Mathf.PI)
-					angleDifference = Mathf.PI * 2.0f + angleDifference;
-
-				//get clockwise or counterclockwise speed
-				float turnRate;
-				if (angleDifference < 0)
-					turnRate = Mathf.Max(angleDifference * Mathf.Rad2Deg, -this.RotationDegreesPerSecond * Easing.Quartic.easeOut(Mathf.Abs(angleDifference / Mathf.PI)) * this._controller.deltaTime);
+				//check if the target angle is inside the back facing dead zone; if so, do nothing
+				float backAngleDifference = Mathf.Atan2(cameraBackDirection.y, cameraBackDirection.x) - targetAngle;
+				if (Mathf.Abs(backAngleDifference) < this.RotationDeadZone * Mathf.Deg2Rad)
+				{
+					//do nothing
+				}
 				else
-					turnRate = Mathf.Min(angleDifference * Mathf.Rad2Deg, this.RotationDegreesPerSecond * Easing.Quartic.easeOut(Mathf.Abs(angleDifference / Mathf.PI)) * this._controller.deltaTime);
+				{
+					//get the angle difference between the target and the camera
+					float angleDifference = currentAngle - targetAngle;
 
-				//HACK
-				this._rotation_speed = turnRate;
+					//wrap it if necessary (should never happen)
+					if (angleDifference > Mathf.PI)
+						angleDifference = angleDifference - Mathf.PI * 2.0f;
 
-				//rotate the camera direction towards the turn direction
-				this._look_direction = Quaternion.AngleAxis(turnRate, this._controller.up) * this._look_direction;
-				transform.rotation = Quaternion.LookRotation(this._look_direction);
+					if (angleDifference < -Mathf.PI)
+						angleDifference = Mathf.PI * 2.0f + angleDifference;
+
+					//get clockwise or counterclockwise speed
+					float turnRate;
+					if (angleDifference < 0)
+						turnRate = Mathf.Max(angleDifference * Mathf.Rad2Deg, -this.RotationDegreesPerSecond * Easing.Quartic.easeOut(Mathf.Abs(angleDifference / Mathf.PI)) * this._controller.deltaTime);
+					else
+						turnRate = Mathf.Min(angleDifference * Mathf.Rad2Deg, this.RotationDegreesPerSecond * Easing.Quartic.easeOut(Mathf.Abs(angleDifference / Mathf.PI)) * this._controller.deltaTime);
+
+					//HACK
+					this._rotation_speed = turnRate;
+
+					//rotate the camera direction towards the turn direction
+					this._look_direction = Quaternion.AngleAxis(turnRate, this._controller.up) * this._look_direction;
+					transform.rotation = Quaternion.LookRotation(this._look_direction);
+				}
 			}
 			else if (Mathf.Abs(this._rotation_speed) > 0.0f)
 			{
