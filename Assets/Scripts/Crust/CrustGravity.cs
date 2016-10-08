a980e9251016e8bcefa1f47fe537646e7289ffab
Jack McCarron <jack.g.mccarron@gmail.com>
2016-10-08 12:49:18 -0700

    scenes with buggy camera movement stuff

--- Assets/Scripts/Crust/CrustGravity.cs
+++ Assets/Scripts/Crust/CrustGravity.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace AssemblyCSharp
 {
 	/// <summary>
-	/// Rotates a this transform to align it towards the target transform's position
+	/// Rotates this transform to align it towards the target transform's position
 	/// </summary>
 	public class CrustGravity : MonoBehaviour
 	{
@@ -15,7 +15,7 @@ namespace AssemblyCSharp
 			Vector3 dir = (this.transform.position - this.Target.position).normalized;
 			this.transform.rotation = Quaternion.FromToRotation(transform.up, dir) * transform.rotation;
 
-			//HACK
+			//HACK HACK HACK
 			Camera.main.transform.rotation = Quaternion.FromToRotation(Camera.main.transform.up, dir) * Camera.main.transform.rotation;
 		}
 	}
