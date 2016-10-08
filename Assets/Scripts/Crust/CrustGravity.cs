d390826a54e0fa16104c8d163b40955cf07e1ea9
Jack McCarron <jack.g.mccarron@gmail.com>
2016-10-08 12:30:27 -0700

    planetoid test

---
+++ Assets/Scripts/Crust/CrustGravity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AssemblyCSharp
+{
+	/// <summary>
+	/// Rotates a this transform to align it towards the target transform's position
+	/// </summary>
+	public class CrustGravity : MonoBehaviour
+	{
+		public Transform Target;
+
+		void Update()
+		{
+			Vector3 dir = (this.transform.position - this.Target.position).normalized;
+			this.transform.rotation = Quaternion.FromToRotation(transform.up, dir) * transform.rotation;
+
+			//HACK
+			Camera.main.transform.rotation = Quaternion.FromToRotation(Camera.main.transform.up, dir) * Camera.main.transform.rotation;
+		}
+	}
+}
